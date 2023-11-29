ff44b7fd4cdb30feaff9f4ec49ee6753b7fa1862
Aman Thukral <thukral618@gmail.com>
2023-11-29 19:13:13 +0530

    Adding UI for whiteboard

--- frontend/src/pages/RoomPage.jsx
+++ frontend/src/pages/RoomPage.jsx
@@ -1,8 +1,92 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import PropTypes from "prop-types";
-
+import WhiteBoard from "../components/WhiteBoard/WhiteBoard";
+import "./styles.css";
 const RoomPage = (props) => {
-  return <div>RoomPage</div>;
+  const [tool, setTool] = useState("pencil");
+  const [color, setColor] = useState("black");
+  const canvasRef = useRef(null);
+  const ctxRef = useRef(null);
+  console.log(typeof canvasRef);
+  return (
+    <div className="">
+      <h1 className="text-center py-5 text-2xl">
+        White Board Sharing App <span>[Users Online : 0]</span>
+      </h1>
+      <div className="flex md:flex-row flex-col mt-4 mb-5 justify-around items-center">
+        <div className="flex lg:w-1/4 w-full justify-center lg:mb-0 mb-3">
+          <div className="flex gap-1 mr-1">
+            <label htmlFor="pencil">Pencil</label>
+            <input
+              type="radio"
+              name="tool"
+              value="pencil"
+              id="pencil"
+              className="mt-1"
+              checked={tool === "pencil"}
+              onChange={(e) => setTool(e.target.value)}
+            />
+          </div>
+          <div className="flex gap-1 mr-1">
+            <label htmlFor="line">Line</label>
+            <input
+              type="radio"
+              name="tool"
+              value="line"
+              id="line"
+              checked={tool === "line"}
+              className="mt-1"
+              onChange={(e) => setTool(e.target.value)}
+            />
+          </div>
+          <div className="flex gap-1 mr-1">
+            <label htmlFor="rect">Rect</label>
+            <input
+              type="radio"
+              name="tool"
+              value="rect"
+              id="rect"
+              checked={tool === "rect"}
+              className="mt-1"
+              onChange={(e) => setTool(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="flex lg:w-1/4 w-full justify-center lg:mb-0 mb-3">
+          <div className="flex gap-2 items-center">
+            <label htmlFor="color">Select Color</label>
+            <input
+              type="color"
+              value={color}
+              id="color"
+              className="mt-1"
+              onChange={(e) => setColor(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="flex lg:w-1/4 w-full justify-center lg:mb-0 mb-3">
+          <div className="flex gap-2 items-center">
+            <button className="text-black px-4 py-1 bg-slate-400 rounded-sm">
+              Undo
+            </button>
+            <button className="px-4 py-1 bg-[#283f4dc2] rounded-sm">
+              Redo
+            </button>
+          </div>
+        </div>
+        <div className="flex lg:w-1/4 w-full justify-center lg:mb-0 mb-3">
+          <div className="flex gap-2 items-center">
+            <button className="px-4 py-1 bg-red-900 rounded-sm">
+              Clear Canvas
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="flex mt-8">
+        <WhiteBoard canvasRef={canvasRef} ctxRef={ctxRef} />
+      </div>
+    </div>
+  );
 };
 
 RoomPage.propTypes = {};
